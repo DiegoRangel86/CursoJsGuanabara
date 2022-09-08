2395292a77df3f0ab036c3fec60ce8c39646f7f2
Diego Rangel da Silva <104211351+DiegoRangel86@users.noreply.github.com>
2022-09-08 15:18:38 -0300

    Fim Aula 11 modulo D

---
+++ aula11/ex009.js
@@ -0,0 +1,17 @@
+
+//Simples
+/*var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h!`)
+
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida, foi multado!')
+}
+console.log('Dirija sempre usando o cinto de segurança')*/
+
+//Composta
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else {
+    console.log('Você é estrangeiro!')
+}
